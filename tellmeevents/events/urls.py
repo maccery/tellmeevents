a94ed089729a5346aacb6006b507302b74405978
Tom Macmichael <tommacmichael@Toms-MBP.home>
2016-01-30 16:31:09 +0000

    You can now select categories, and the events returned will be from them

--- tellmeevents/events/urls.py
+++ tellmeevents/events/urls.py
@@ -1,8 +1,7 @@
 from django.conf.urls import url
-
 from . import views
+from views import CategoriesView
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
-    url(r'^events$', views.events, name='events'),
+    url(r'^$', CategoriesView.as_view()),
 ]
--- tellmeevents/events/views.py
+++ tellmeevents/events/views.py
@@ -1,17 +1,19 @@
 from django.shortcuts import render_to_response
 from django.http import HttpResponse
+from django.views.generic import View
+from django.template import RequestContext
 import services
 
 
 # Create your views here.
-def index(request):
-    categories = services.get_categories()['categories']
+class CategoriesView(View):
+    def get(self, request):
+        categories = services.get_categories()['categories']
 
-    return render_to_response('events/categories.html', {'categories': categories})
+        return render_to_response('events/categories.html', {'categories': categories}, context_instance=RequestContext(request))
 
+    def post(self, request):
+        category_ids = request.POST.getlist('categories[]')
+        events = services.get_events(category_ids)['events']
 
-def events(request):
-    category_ids = [108]
-    events = services.get_events(category_ids)['events']
-
-    return render_to_response('events/events.html', {'events': events})
+        return render_to_response('events/events.html', {'events': events, 'category_ids': category_ids}, context_instance=RequestContext(request))
