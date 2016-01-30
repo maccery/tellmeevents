a9d6e87b50f73d97bf67b1c5967b06c915a7d767
Tom Macmichael <tommacmichael@Toms-MBP.home>
2016-01-30 16:46:47 +0000

    Now the entire category objects are given when searching, so more detail can be displayed on results page

--- tellmeevents/events/services.py
+++ tellmeevents/events/services.py
@@ -3,16 +3,27 @@ from __future__ import unicode_literals
 from django.db import models
 from eventbrite import Eventbrite
 
-eventbrite = Eventbrite('5NGHYUHHECWB3CBRXKY2')
 
-def get_categories():
+def get_all_categories():
     """
     This returns all categories from the eventbrite API
 
+    :return:
     :return: List of categories
     """
     eventbrite = Eventbrite('5NGHYUHHECWB3CBRXKY2')
     return eventbrite.get_categories()
+
+
+def get_category(category_id):
+    """
+    Given the id of a category, returns that eventbrite category object
+
+    :param category_id:
+    :return:
+    """
+    eventbrite = Eventbrite('5NGHYUHHECWB3CBRXKY2')
+    return eventbrite.get_category(category_id)
 
 
 def get_events(category_ids):
@@ -22,5 +33,5 @@ def get_events(category_ids):
     :return: List of events
     :param categories:
     """
-
+    eventbrite = Eventbrite('5NGHYUHHECWB3CBRXKY2')
     return eventbrite.event_search(**{'categories': category_ids})
--- tellmeevents/events/views.py
+++ tellmeevents/events/views.py
@@ -8,12 +8,19 @@ import services
 # Create your views here.
 class CategoriesView(View):
     def get(self, request):
-        categories = services.get_categories()['categories']
+        categories = services.get_all_categories()['categories']
 
-        return render_to_response('events/categories.html', {'categories': categories}, context_instance=RequestContext(request))
+        return render_to_response('events/categories.html', {'categories': categories},
+                                  context_instance=RequestContext(request))
 
     def post(self, request):
         category_ids = request.POST.getlist('categories[]')
         events = services.get_events(category_ids)['events']
 
-        return render_to_response('events/events.html', {'events': events, 'category_ids': category_ids}, context_instance=RequestContext(request))
+        # Loop through the category ids we've been given and get the eventbrite object
+        categories = []
+        for category_id in category_ids:
+            categories.append(services.get_category(category_id))
+
+        return render_to_response('events/events.html', {'events': events, 'categories': categories},
+                                  context_instance=RequestContext(request))
