df0412be134d38e012fc276c96b3d2d57da3c9d9
Tom Macmichael <tommacmichael@Toms-MBP.home>
2016-01-30 15:05:22 +0000

    Events, categories page are now separate. Updated URLs

--- tellmeevents/events/services.py
+++ tellmeevents/events/services.py
@@ -3,29 +3,24 @@ from __future__ import unicode_literals
 from django.db import models
 from eventbrite import Eventbrite
 
+eventbrite = Eventbrite('5NGHYUHHECWB3CBRXKY2')
 
 def get_categories():
     """
     This returns all categories from the eventbrite API
 
-    :return:
+    :return: List of categories
     """
     eventbrite = Eventbrite('5NGHYUHHECWB3CBRXKY2')
-    categories = eventbrite.get_categories()
-
-    return get_events(categories)
+    return eventbrite.get_categories()
 
 
-def get_events(categories):
+def get_events(category_ids):
     """
     Given a set of categories, returns relevant events
 
+    :return: List of events
     :param categories:
     """
-    eventbrite = Eventbrite('5NGHYUHHECWB3CBRXKY2')
-    category_ids = []
-    for category in categories['categories']:
-        category_ids = category['id']
 
-    events = eventbrite.event_search(**{'categories': 108})
-    return events
+    return eventbrite.event_search(**{'categories': category_ids})
--- tellmeevents/events/views.py
+++ tellmeevents/events/views.py
@@ -1,8 +1,17 @@
-from django.shortcuts import render, render_to_response
+from django.shortcuts import render_to_response
 from django.http import HttpResponse
 import services
 
+
 # Create your views here.
 def index(request):
-    categories = services.get_categories()['events']
+    categories = services.get_categories()['categories']
+
     return render_to_response('events/categories.html', {'categories': categories})
+
+
+def events(request):
+    category_ids = [108]
+    events = services.get_events(category_ids)['events']
+
+    return render_to_response('events/events.html', {'events': events})
