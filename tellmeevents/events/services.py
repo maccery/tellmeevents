b4ebf18ee7e87c718700b048a973c9de2dbba29c
Tom Macmichael <tommacmichael@Toms-MBP.home>
2016-01-30 14:57:57 +0000

    Getting events, given category ids, now working

--- tellmeevents/events/services.py
+++ tellmeevents/events/services.py
@@ -5,6 +5,27 @@ from eventbrite import Eventbrite
 
 
 def get_categories():
+    """
+    This returns all categories from the eventbrite API
+
+    :return:
+    """
     eventbrite = Eventbrite('5NGHYUHHECWB3CBRXKY2')
     categories = eventbrite.get_categories()
-    return categories
+
+    return get_events(categories)
+
+
+def get_events(categories):
+    """
+    Given a set of categories, returns relevant events
+
+    :param categories:
+    """
+    eventbrite = Eventbrite('5NGHYUHHECWB3CBRXKY2')
+    category_ids = []
+    for category in categories['categories']:
+        category_ids = category['id']
+
+    events = eventbrite.event_search(**{'categories': 108})
+    return events
--- tellmeevents/events/views.py
+++ tellmeevents/events/views.py
@@ -1,8 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, render_to_response
 from django.http import HttpResponse
 import services
 
 # Create your views here.
 def index(request):
-    categories = services.get_categories()
-    return render(request, 'events/categories.html', categories)
+    categories = services.get_categories()['events']
+    return render_to_response('events/categories.html', {'categories': categories})
