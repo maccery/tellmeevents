e087de03293714fdc869b92ce222ca71082b6d00
Tom Macmichael <tommacmichael@Toms-MacBook-Pro.local>
2016-02-01 14:22:07 +0000

    Split code up a bit more

--- events/services.py
+++ events/services.py
@@ -1,43 +1,64 @@
 from __future__ import unicode_literals
 
-from django.db import models
 from django.conf import settings
 from eventbrite import Eventbrite
 
 
-def get_all_categories():
-    """
-    This returns all categories from the eventbrite API
+class Category:
+    def __init__(self):
+        self.eventbrite = Eventbrite(settings.EVENTBRITE_API_KEY)
 
-    :return:
-    :return: List of categories
-    """
-    eventbrite = Eventbrite(settings.EVENTBRITE_API_KEY)
-    return eventbrite.get_categories()
+    def get_all_categories(self):
+        """
+        This returns all categories from the eventbrite API
+
+        :return:
+        :return: List of categories
+        """
+        return self.eventbrite.get_categories()['categories']
+
+    def get_category(self, category_id):
+        """
+        Given the id of a category, returns that eventbrite category object
+
+        :param category_id:
+        :return:
+        """
+        return self.eventbrite.get_category(category_id)
+
+    def get_categories(self, category_ids):
+        """
+        Returns category objects for category ids given
+
+        :param category_ids:
+        :return:
+        """
+        categories = []
+        for category_id in category_ids:
+            categories.append(self.get_category(category_id))
+
+        return categories
 
 
-def get_category(category_id):
-    """
-    Given the id of a category, returns that eventbrite category object
+class Event:
+    def __init__(self):
+        self.eventbrite = Eventbrite(settings.EVENTBRITE_API_KEY)
 
-    :param category_id:
-    :return:
-    """
-    eventbrite = Eventbrite(settings.EVENTBRITE_API_KEY)
-    return eventbrite.get_category(category_id)
+    def get_event(self, events_id):
+        """
+        Given an eventbrite ID, returns the event object
 
+        :param events_id:
+        :return:
+        """
+        return self.get_event(events_id)
 
-def get_event(events_id):
-    eventbrite = Eventbrite(settings.EVENTBRITE_API_KEY)
-    return eventbrite.get_event(events_id)
+    def get_events(self, category_ids, page_number):
+        """
+        Given a set of categories, returns relevant events
 
+        :return: List of events
+        :param categories:
+        """
 
-def get_events(category_ids, page_number):
-    """
-    Given a set of categories, returns relevant events
-
-    :return: List of events
-    :param categories:
-    """
-    eventbrite = Eventbrite(settings.EVENTBRITE_API_KEY)
-    return eventbrite.event_search(**{'page': page_number, 'categories': category_ids})
+        return self.eventbrite.event_search(**{'page': page_number, 'categories': category_ids})
--- events/views.py
+++ events/views.py
@@ -1,37 +1,35 @@
 from django.shortcuts import render_to_response
-from django.http import HttpResponse
 from django.views.generic import View
 from django.template import RequestContext
-import services
+from services import Category, Event
 
 
-# Create your views here.
 class CategoriesView(View):
     def get(self, request):
-        categories = services.get_all_categories()['categories']
+        categories = Category().get_all_categories()
 
-        return render_to_response('events/categories.html', {'categories': categories},
-                                  context_instance=RequestContext(request))
+        return render_to_response('events/categories.html', {'categories': categories})
+
 
 class EventView(View):
     def get(self, request, events_id):
-        event = services.get_event(events_id)
+        event = Event().get_event(events_id)
 
         return render_to_response('events/event.html', {'event': event})
 
+
 class ResultsView(View):
     def get(self, request, page_number=1):
-        category_ids = [request.GET.get('category1', ''), request.GET.get('category2', ''), request.GET.get('category3', '')]
+        category_ids = [request.GET.get('category1', ''), request.GET.get('category2', ''),
+                        request.GET.get('category3', '')]
 
-        data = services.get_events(category_ids, page_number)
-        page_count = data['pagination']['page_count']
-
+        data = Event().get_events(category_ids, page_number)
+        categories = Category().get_categories(category_ids)
         events = data['events']
+        total_pages = data['pagination']['page_count']
 
-        # Loop through the category ids we've been given and get the eventbrite object
-        categories = []
-        for category_id in category_ids:
-            categories.append(services.get_category(category_id))
-
-        return render_to_response('events/events.html', {'events': events, 'range': range(1, page_count), 'page_count': page_count, 'page_number': page_number},
-                                  context_instance=RequestContext(request))
+        return render_to_response('events/events.html',
+                                  {'events': events, 'categories': categories,
+                                   'range': range(1, total_pages),
+                                   'page_count': total_pages,
+                                   'page_number': page_number})
